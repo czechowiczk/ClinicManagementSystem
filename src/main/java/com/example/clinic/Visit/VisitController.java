3a76f056b3f04726c3742126ae66825ebbd517ed
cjavaprog <konrad.czechowicz22@gmail.com>
2021-01-12 22:18:07 +0100

    added visitController

---
+++ src/main/java/com/example/clinic/Visit/VisitController.java
@@ -0,0 +1,11 @@
+package com.example.clinic.Visit;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("visit")
+public class VisitController {
+
+
+}
