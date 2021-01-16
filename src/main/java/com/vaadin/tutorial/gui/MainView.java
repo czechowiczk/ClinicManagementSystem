0a4cb24467da238db7227b12c464957b35ec481e
Bartek <32011226+czuptab@users.noreply.github.com>
2021-01-16 13:59:26 +0100

    Added gitignore

--- src/main/java/com/vaadin/tutorial/gui/MainView.java
+++ src/main/java/com/vaadin/tutorial/gui/MainView.java
@@ -1,7 +1,9 @@
 package com.vaadin.tutorial.gui;
 
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.tutorial.backend.entity.Company;
@@ -25,17 +27,35 @@ public class MainView extends VerticalLayout {
         this.contactService = contactService;
         addClassName("list-view"); //nazwa klasy do css
         setSizeFull();
+
         configureGrid();
-        configureFilter();
 
         form = new ContactForm(companyService.findAll());
+        form.addListener(ContactForm.SaveEvent.class, this::saveContact);
+        form.addListener(ContactForm.DeleteEvent.class, this::deleteContact);
+        form.addListener(ContactForm.CloseEvent.class, e -> closeEditor());
+
 
         Div content = new Div(grid, form);
         content.addClassName("content");
         content.setSizeFull();
 
-        add(filterText, content);
+        add(getToolbar(), content);
         updateList();
+
+        closeEditor();
+    }
+
+    private void saveContact(ContactForm.SaveEvent event) {
+        contactService.save(event.getContact());
+        updateList();
+        closeEditor();
+    }
+
+    private void deleteContact(ContactForm.DeleteEvent event) {
+        contactService.delete(event.getContact());
+        updateList();
+        closeEditor();
     }
 
     private void configureGrid() {
@@ -50,13 +70,46 @@ public class MainView extends VerticalLayout {
         }).setHeader("Company");
 
         grid.getColumns().forEach(col -> col.setAutoWidth((true)));
+
+        grid.asSingleSelect().addValueChangeListener(event ->
+                editContact(event.getValue()));
     }
 
-    private void configureFilter() {
+    private void editContact(Contact contact) {
+        if (contact == null) {
+            closeEditor();
+        } else {
+            form.setContact(contact);
+            form.setVisible(true);
+            addClassName("editing");
+        }
+    }
+
+    private void closeEditor() {
+        form.setContact(null);
+        form.setVisible(false);
+        removeClassName("editing");
+    }
+
+    private HorizontalLayout getToolbar() {
         filterText.setPlaceholder("Filter by name...");
         filterText.setClearButtonVisible(true);
         filterText.setValueChangeMode(ValueChangeMode.LAZY);
         filterText.addValueChangeListener(e -> updateList());
+
+        Button addContactButton = new Button("Add contact");
+        addContactButton.addClickListener(click -> addContact());
+
+        HorizontalLayout toolbar = new HorizontalLayout(filterText, addContactButton);
+
+
+        toolbar.addClassName("toolbar");
+        return toolbar;
+    }
+
+    void addContact() {
+        grid.asSingleSelect().clear();
+        editContact(new Contact());
     }
 
     private void updateList() {
