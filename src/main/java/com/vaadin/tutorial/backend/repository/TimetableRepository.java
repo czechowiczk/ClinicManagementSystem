fd2ef6213e99aabf09fe1f0f577eda923e1eb261
cjavaprog <konrad.czechowicz22@gmail.com>
2021-01-18 19:16:29 +0100

    minor bugs fixed

--- src/main/java/com/vaadin/tutorial/backend/repository/TimetableRepository.java
+++
@@ -1,8 +0,0 @@
-package com.vaadin.tutorial.backend.repository;
-
-import com.vaadin.tutorial.backend.entity.Timetable;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TimetableRepository extends JpaRepository<Timetable, Integer> {
-
-}
